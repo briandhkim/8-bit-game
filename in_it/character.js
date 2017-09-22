15d3243cb97ce6b6bf1d6a689986a2bd59cbc30a
michaeljlin <mjameslin@gmail.com>
2017-09-22 13:14:07 -0700

    Added instructions on intended usage + basic return expectations

--- in_it/character.js
+++ in_it/character.js
@@ -1,3 +1,14 @@
+// Character Constructor
+//
+// Example Usage:
+// Name = Pharah
+// Health = 100
+// Image = 'pharah.png'
+// Skills = [ move1Object, move2Object, move3Object, move4Object ]
+//
+// In code form:
+// var pharah = new character('Pharah', 100, 'pharah.png', skillArray);
+//
 function character(name, health, image, skills){
     this.self = this;
 
@@ -9,14 +20,14 @@ function character(name, health, image, skills){
     this.alive = true;
 
     this.getHealth = function(){
-
+        return self.health;
     };
 
     this.getName = function(){
-
+        return self.name;
     };
 
-    this.useSkill = function(){
-
+    this.useSkill = function(moveNumber){
+        return skills[moveNumber];
     }
 }
