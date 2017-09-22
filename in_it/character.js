6c696dc35874bec376c333ecd22c35e4ca121462
michaeljlin <mjameslin@gmail.com>
2017-09-22 13:05:05 -0700

    Basic constructor skeleton done

---
+++ in_it/character.js
@@ -0,0 +1,22 @@
+function character(name, health, image, skills){
+    this.self = this;
+
+    this.name = name;
+    this.health = health;
+    this.image = image;
+    this.skills = skills;
+
+    this.alive = true;
+
+    this.getHealth = function(){
+
+    };
+
+    this.getName = function(){
+
+    };
+
+    this.useSkill = function(){
+
+    }
+}
