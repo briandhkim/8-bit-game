1739cdf4fdb095005ed1f51cbe6bf03c51fe2623
briandhkim <w1057216@gmail.com>
2017-09-25 00:20:22 -0700

    stat aoe and heal edit to number from boolean

--- in_it/charStats.js
+++ in_it/charStats.js
@@ -8,6 +8,9 @@
 
 	for testing, use hanzo, mccree, genji, torbjorn, or tank units
 	they shouldn't have healing condition conflicts
+
+	aoe may need to be boolean to make it impact healing factor too.
+	as of now, aoe is just the damage that other characters would take
 */
 
 
@@ -21,32 +24,40 @@ var ana = {
 		damage: 60,
 		accuracy: 60,	//out of 100
 		pp: 10,
-		aoe: false,		//skills with aoe true can do maybe 50% of damage to opponent characters on reserve
-		heal: true
+		// aoe: false,		//skills with aoe true can do maybe 50% of damage to opponent characters on reserve
+		aoe: 0,
+		// heal: true
+		heal: 75
 	},
 	skill_2 :{
 		name: 'Sleep Dart', //maybe skip this skill for the time, this would stun the character that is hit
 		damage: 5,
 		accuracy: 20,
 		pp: 2,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Biotic Grenade',
 		damage: 60,
 		accuracy: 90,
 		pp: 3,
-		aoe: true,
-		heal: true
-	}
+		// aoe: true,
+		aoe: 30,
+		// heal: true
+		heal: 80 //might change to 100
+	},
 	skill_4 :{
 		name: 'Nano Boost',	//choose an ally at random and incresae their damage at next turn by 50%
 		damage: 0,			//ignore this skill until mechanics are worked out
 		accuracy: 100,
 		pp: 1,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -60,32 +71,40 @@ var lucio = {
 		damage: 20,
 		accuracy: 70,
 		pp: 20,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Soundwave',
 		damage: 25,
 		accuracy: 90,
 		pp: 5,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 45,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Amp It Up',
 		damage: 0,
 		accuracy: 100,
 		pp: 5,
-		aoe: true,
-		heal: true 	//heals all allies alive by 50hp
+		// aoe: true,
+		aoe: 0,
+		//heal: true 	//heals all allies alive by 50hp
+		heal: 60
 	},
 	skill_4 :{
 		name: 'Sound Barrier',
 		damage: 0,
 		accuracy: 100,
 		pp: 1,
-		aoe: true,
-		heal: true	//heals all allies alive 100%
+		// aoe: true,
+		aoe: 0,
+		//heal: true	//heals all allies alive 100%
+		heal: 200
 	}
 };
 
@@ -99,32 +118,40 @@ var zenyatta = {
 		damage: 46,
 		accuracy: 80,
 		pp: 20,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Orb Volley',
 		damage: 230,
 		accuracy: 30,
 		pp: 4,	//maybe decrease down to 3
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Orb of Harmony',	//heal one ally chosen at random by 50hp
 		damage: 0,				//the ally must be alive
 		accuracy: 100,
 		pp: 4,
-		aoe: false,
-		heal: true
+		// aoe: false,
+		aoe: 0,
+		// heal: true
+		heal: 50
 	},
 	skill_4 :{
 		name: 'Transcendence',	//heal all allies alive 100%
 		damage: 0,
 		accuracy: 100,
 		pp: 1,
-		aoe: true,
-		heal: true
+		// aoe: true,
+		aoe: 0,
+		// heal: true
+		heal: 200	
 	}
 };
 
@@ -138,32 +165,40 @@ var dva = {
 		damage: 22,
 		accuracy: 90,
 		pp: 25,
-		aoe: true,		//might be false
-		heal: false
+		//aoe: true,		//might be false
+		aoe: 20,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Micro Missiles',
 		damage: 66,		//might have to increase
 		accuracy: 50,
 		pp: 5,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Boost',
 		damage: 25,
 		accuracy: 60,
 		pp: 5,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Self-Destruct',
-		damage: 1000,	//may need to be decreased
+		damage: 1000,	//may need to be decreased or decrease accuracy
 		accuracy: 95,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 500,	//probably needs to be decreased
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -177,32 +212,40 @@ var reinhardt = {
 		damage: 75,
 		accuracy: 95,
 		pp: 30,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 37,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Charge',
 		damage: 200,
 		accuracy: 50,
 		pp: 5,
-		aoe: false,	//may need to change to true
-		heal: false
+		//aoe: false,	//may need to change to true
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Fire Strike',
 		damage: 100,
 		accuracy: 70,
 		pp: 7,
-		aoe: false, //may need to change to true
-		heal: false
+		//aoe: false, //may need to change to true
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Earthshatter',	//may need to add stun effect
 		damage: 50,
 		accuracy: 80,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 35,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -216,32 +259,40 @@ var roadhog = {
 		damage: 25,
 		accuracy: 80,
 		pp: 5,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 15,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Scrap Gun - LR',
 		damage: 50,
 		accuracy: 50,
 		pp: 5,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Chain Hook',
 		damage: 30,
 		accuracy: 60,
 		pp: 5,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Whole Hog',
 		damage: 2500,
 		accuracy: 25,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 1250, // will probably most definitely need to be decreased
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -255,32 +306,40 @@ var hanzo = {
 		damage: 90,
 		accuracy: 35,
 		pp: 20,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Sonic Arrow',
 		damage: 90,
 		accuracy: 50,
 		pp: 5,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Scatter Arrow',
-		damage: 25,
+		damage: 40,
 		accuracy: 50,
 		pp: 4,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 20,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Ryū ga waga teki wo kurau!',
 		damage: 200,
 		accuracy: 75,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 100,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -294,32 +353,40 @@ var junkrat = {
 		damage: 40,
 		accuracy: 75,
 		pp: 5,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 20,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Concussion Mine',
 		damage: 120,
 		accuracy: 40,
 		pp: 2,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 60,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Steel Trap',	//consider stun option
 		damage: 80,
 		accuracy: 85,
 		pp: 1,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'RiP-Tire',
 		damage: 400,
 		accuracy: 65,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 200,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -333,32 +400,40 @@ var mei = {
 		damage: 30,
 		accuracy: 95,
 		pp: 10,	//maybe increase to 20
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 15,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Icicle',
 		damage: 75,
 		accuracy: 50,
 		pp: 8,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Cryo-Freeze',
 		damage: 0,
 		accuracy: 100,
 		pp: 4,
-		aoe: false,
-		heal: true 	//heals self 100hp
+		// aoe: false,
+		aoe: 0,
+		//heal: true 	//heals self 100hp
+		heal: 150
 	},
 	skill_4 :{
 		name: 'Blizzard',
-		damage: 97,
+		damage: 100,
 		accuracy: 90,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 50,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -372,32 +447,40 @@ var torbjorn = {
 		damage: 70,
 		accuracy: 60,
 		pp: 18,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Rivet Shot Gun',
 		damage: 15,
 		accuracy: 80,
 		pp: 6,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 8,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Build Turret',
 		damage: 14,
 		accuracy: 100,
 		pp: 5,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Molten Core',
 		damage: 110,		//hammer damage*2 for the time
 		accuracy: 95,
 		pp: 1,
-		aoe: false,
-		heal: true  //only heals self 100% hp
+		// aoe: false,
+		aoe: 0,
+		//heal: true  //only heals self 100% hp
+		heal: 200
 	}
 };
 
@@ -411,32 +494,40 @@ var genji = {
 		damage: 60,
 		accuracy: 60,
 		pp: 8,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Fan of Blades',
 		damage: 60,
 		accuracy: 70,
 		pp: 8,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 30,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Swift Strike',
 		damage: 50,
 		accuracy: 70,
 		pp: 3,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 25,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Ryūjin no ken wo kurae',
 		damage: 120,
 		accuracy: 85,
 		pp: 1,
-		aoe: false,		//maybe change to true
-		heal: false
+		//aoe: false,		//maybe change to true
+		aoe: 0,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -450,32 +541,40 @@ var mccree = {
 		damage: 45,
 		accuracy: 65,
 		pp: 6,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Fan the Hammer',
 		damage: 30,
 		accuracy: 80,
 		pp: 6,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 15,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Flashbang',		//consider adding stun effect
 		damage: 25,
 		accuracy: 85,
 		pp: 3,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 13,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: "It's 420 Noon",
 		damage: 250,
 		accuracy: 80,		//may need to adjust
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 125,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -489,32 +588,40 @@ var pharah = {
 		damage: 120,
 		accuracy: 80,
 		pp: 6,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 60,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Jump Jet',
 		damage: 0,
 		accuracy: 100,
 		pp: 2,
-		aoe: false,
-		heal: true 		//heal self 50hp for the time, maybe change to no heal and evading next attack
+		// aoe: false,
+		aoe: 0,
+		//heal: true 		//heal self 50hp for the time, maybe change to no heal and evading next attack
+		heal: 50
 	},
 	skill_3 :{
 		name: 'Concussive Blast',
 		damage: 10,
 		accuracy: 80,
 		pp: 5,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 5,
+		// heal: false
+		heal: 0
 	},
 	skill_4 :{
 		name: 'Barrage',
 		damage: 120,
 		accuracy: 85,
 		pp: 1,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 60,
+		// heal: false
+		heal: 0
 	}
 };
 
@@ -528,32 +635,40 @@ var soldier = {
 		damage: 19,
 		accuracy: 75,
 		pp: 25,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	},
 	skill_2 :{
 		name: 'Helix Rockets',
 		damage: 60,
 		accuracy: 70,
 		pp: 5,
-		aoe: true,
-		heal: false
+		// aoe: true,
+		aoe: 30,
+		// heal: false
+		heal: 0
 	},
 	skill_3 :{
 		name: 'Biotic Field',
 		damage: 0,
 		accuracy: 100,
 		pp: 4,
-		aoe: true,
-		heal: true  	//heal self and ally 50hp
+		// aoe: true,
+		aoe: 0,
+		//heal: true  	//heal self and ally 50hp
+		heal: 100
 	},
 	skill_4 :{
 		name: 'Tactical Visor',
 		damage: 70,
 		accuracy: 100,
 		pp: 2,
-		aoe: false,
-		heal: false
+		// aoe: false,
+		aoe: 0,
+		// heal: false
+		heal: 0
 	}
 };
 
