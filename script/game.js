e167dd2352408734ca57f141a5afb9bbf30573f9
briandhkim <w1057216@gmail.com>
2018-01-04 11:19:17 -0800

    minor script changes in game.js

--- script/game.js
+++ script/game.js
@@ -105,11 +105,9 @@ function Game(uiUpdater){
             prevTurnMsg = `Player ${this.currentPlayerTurn+1} switched to ${currentPlayer.activeCharacter.name}`;
             uiUp.changeCharacterUpdate(currentPlayer, this.currentPlayerTurn);
             this.changePlayerTurn();
-        }else if(!charChangeCheck){
+        }else{
             const consoleMsg = "You can't select this character...";
             uiUp.updateConsoleCustomMsg(consoleMsg);
-        }else{
-            console.log('error in turnChangeChar in game obj');
         }
     };
     /***************************
@@ -136,7 +134,7 @@ function Game(uiUpdater){
             setTimeout(function(){
                 this.buttonRebind();
             }.bind(this),950);
-            
+
             const opponentNum = 1-this.currentPlayerTurn;
             //if currentPlayerTurn=1, opponentNum=0 else cPT=0, oppNum=1
             const opponentPlayer = `player${opponentNum+1}`;
@@ -168,13 +166,7 @@ function Game(uiUpdater){
     descpt: returns false if character is alive; true if dead
     */
     this.checkCharDead = function(character){
-        if(character.alive){
-            return false;
-        }else if(!character.alive){
-            return true;
-        }else{
-            console.log('error checkCharDead function');
-        }
+        return !character.alive;
     };
     /***************************
     deadCharSwap -> 
